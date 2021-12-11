b4078082498fd1c4fe3e4a9485b85473a7b43751
stevy Endaman <stevyabessolo@gmail.com>
2021-12-11 09:56:31 +0100

    Update RolesTableSeeder.php

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -14,12 +14,12 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-
+        
 
         \DB::table('roles')->delete();
-
+        
         \DB::table('roles')->insert(array (
-            0 =>
+            0 => 
             array (
                 'id' => 1,
                 'name' => 'admin',
@@ -27,7 +27,7 @@ class RolesTableSeeder extends Seeder
                 'created_at' => '2021-11-28 19:55:14',
                 'updated_at' => '2021-11-28 19:55:14',
             ),
-            1 =>
+            1 => 
             array (
                 'id' => 2,
                 'name' => 'user',
@@ -35,16 +35,16 @@ class RolesTableSeeder extends Seeder
                 'created_at' => '2021-11-28 19:55:14',
                 'updated_at' => '2021-11-28 19:55:14',
             ),
-            3 =>
-                array (
-                    'id' => 3,
-                    'name' => 'seller',
-                    'display_name' => 'Seller',
-                    'created_at' => '2021-11-28 19:55:14',
-                    'updated_at' => '2021-11-28 19:55:14',
-                ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => 'seller',
+                'display_name' => 'Seller',
+                'created_at' => '2021-11-28 19:55:14',
+                'updated_at' => '2021-11-28 19:55:14',
+            ),
         ));
-
-
+        
+        
     }
 }
